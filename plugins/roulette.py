bd36076aa25c6a8042ee97a487dcfbe9f9be6721
PC <pc@kvalvik.no>
2016-02-17 23:18:16 +0100

    Complete overhaul and add !hotpotato

--- plugins/roulette.py
+++ plugins/roulette.py
@@ -1,9 +1,11 @@
 """ Script for Russian Roulette
 
-Commands: none
+Commands:
+!roulette
 """
 
-from random import randint, shuffle
+from random import randint, choice
+from threading import Timer
 
 import discord
 import asyncio
@@ -19,6 +21,155 @@ commands = {
 
 # List containing all channels running !roulette
 started = []
+
+
+class Roulette:
+    name = "Russian Roulette"
+    min_num = 1
+
+    def __init__(self, client: discord.Client, message: discord.Message, num: int):
+        self.client = client
+        self.message = message
+        self.member = message.server.get_member(client.user.id)
+
+        self.num = num if num >= self.min_num else self.min_num
+        self.participants = []
+        self.bullets = []
+
+    def on_start(self):
+        m = "{} has started a game of {}! To participate, say `I`! {} players needed.".format(
+            self.message.author.mention, self.name, self.num)
+
+        yield from self.client.send_message(self.message.channel, m)
+
+    def get_participants(self):
+        for i in range(self.num):
+            def check(m):
+                if m.content.lower().strip() == "i" and m.author.id not in self.participants:
+                    return True
+
+                return False
+
+            reply = yield from self.client.wait_for_message(timeout=120, channel=self.message.channel, check=check)
+
+            if reply:
+                asyncio.async(
+                    self.client.send_message(self.message.channel,
+                                             "{} has entered! `{}/{}`. "
+                                             "Type `I` to join!".format(reply.author.mention, i+1, self.num)))
+                self.participants.append(reply.author.id)
+                if self.member.permissions_in(self.message.channel).manage_messages:
+                    asyncio.async(self.client.delete_message(reply))
+            else:
+                yield from self.client.send_message(self.message.channel,
+                                                    "**The {} game failed to gather"
+                                                    " {} participants.**".format(
+                                                        self.name, self.num
+                                                    ))
+                started.pop(started.index(self.message.channel.id))
+
+                return False
+
+    def shuffle(self):
+        self.bullets = [0] * len(self.participants)
+        self.bullets[randint(0, len(self.participants)-1)] = 1
+
+    def game(self):
+        for i, participant in enumerate(self.participants):
+            member = self.message.server.get_member(participant)
+
+            yield from self.client.send_message(
+                self.message.channel,
+                "{} is up next! Say `go` whenever you are ready.".format(member.mention)
+            )
+            reply = yield from self.client.wait_for_message(timeout=15, channel=self.message.channel, author=member,
+                                                            check=lambda m: "go" in m.content.lower())
+
+            hit = ":dash:"
+
+            if self.bullets[i] == 1 or reply is None:
+                hit = ":boom:"
+
+            if reply is None:
+                yield from self.client.send_message(self.message.channel, "*fuck you*")
+
+            yield from self.client.send_message(self.message.channel, "{} {} :gun: ".format(member.mention, hit))
+
+            if self.bullets[i] == 1:
+                break
+
+        yield from self.client.send_message(self.message.channel, "**GAME OVER**")
+
+        started.pop(started.index(self.message.channel.id))
+
+    def start(self):
+        yield from self.on_start()
+        valid = yield from self.get_participants()
+        if valid is not False:
+            self.shuffle()
+            yield from self.game()
+
+
+class HotPotato(Roulette):
+    name = "Hot Potato"
+    min_num = 3
+
+    def __init__(self, client: discord.Client, message: discord.Message, num: int):
+        super().__init__(client, message, num)
+
+        self.time_remaining = 0
+
+        del self.bullets
+
+    def shuffle(self):
+        pass
+
+    def timer(self):
+        self.time_remaining -= 1
+        if self.time_remaining > 0:
+            Timer(1, self.timer).start()
+
+    def game(self):
+        self.time_remaining = randint(
+            int(pow(7 * len(self.participants), 0.8)),
+            int(pow(15 * len(self.participants), 0.8))
+        )
+
+        print(self.time_remaining)
+
+        participant = choice(self.participants)
+        Timer(1, self.timer).start()
+        reply = True
+
+        while self.time_remaining > 0:
+            member = self.message.server.get_member(participant)
+            pass_to = [choice(self.participants), choice(self.participants)]
+
+            if reply is not None:
+                yield from self.client.send_message(
+                    self.message.channel,
+                    "{} :bomb: got the bomb! Pass it to either {} or {}!".format(
+                        member.mention,
+                        self.message.server.get_member(pass_to[0]).mention,
+                        self.message.server.get_member(pass_to[1]).mention
+                    )
+                )
+
+            wait = (self.time_remaining - 5) if (self.time_remaining > 5) else self.time_remaining
+            reply = yield from self.client.wait_for_message(timeout=wait, channel=self.message.channel, author=member,
+                                                            check=lambda m: m.mentions[0].id in pass_to)
+
+            if reply:
+                participant = reply.mentions[0].id
+            elif self.time_remaining == 5:
+                asyncio.async(self.client.send_message(self.message.channel, ":bomb: :fire: **IT'S GONNA BLOW!**"))
+
+        yield from self.client.send_message(self.message.channel, "{} :fire: :boom: :boom: :fire:".format(
+            self.message.server.get_member(participant).mention
+        ))
+        yield from self.client.send_message(self.message.channel, "**GAME OVER**")
+
+        started.pop(started.index(self.message.channel.id))
 
 
 @asyncio.coroutine
@@ -37,65 +188,28 @@ def on_message(client: discord.Client, message: discord.Message, args: list):
             if num < 1:
                 num = 6
 
-            yield from client.send_message(message.channel,
-                                           "{} has started a game of Russian Roulette! To participate,"
-                                           " say `I`! {} players needed.".format(message.author.mention, num))
+            roulette = Roulette(client, message, num)
 
-            # List containing participant user ids
-            participants = []
-
-            for i in range(num):
-                def check(m):
-                    if m.content.lower().strip() == "i" and m.author.id not in participants:
-                        return True
-
-                    return False
-
-                reply = yield from client.wait_for_message(timeout=120, channel=message.channel, check=check)
-
-                if reply:
-                    asyncio.async(
-                        client.send_message(message.channel,
-                                            "{} has entered! `{}/{}`. "
-                                            "Type `I` to join!".format(reply.author.mention, i+1, num)))
-                    participants.append(reply.author.id)
-                    if message.server.get_member(client.user.id).permissions_in(message.channel).manage_messages:
-                        asyncio.async(client.delete_message(reply))
-                else:
-                    yield from client.send_message(message.channel, "**The Russian Roulette game failed to gather "
-                                                                    "{} participants.**".format(num))
-                    started.pop(started.index(message.channel.id))
-
-                    return
-
-            # Set random order of participants and add one bullet
-            # shuffle(participants)  # We don't want to shuffle the participants, necessarily
-            bullets = [0] * num
-            bullets[randint(0, num-1)] = 1
-
-            for i, participant in enumerate(participants):
-                member = message.server.get_member(participant)
-
-                yield from client.send_message(message.channel,
-                                               "{} is up next! Say `go` whenever you are ready.".format(member.mention))
-                _ = yield from client.wait_for_message(timeout=15, channel=message.channel, author=member,
-                                                       check=lambda m: "go" in m.content.lower())
-
-                hit = ":dash:"
-
-                if bullets[i] == 1 or _ is None:
-                    hit = ":boom:"
-
-                if _ is None:
-                    yield from client.send_message(message.channel, "*fuck you*")
-
-                yield from client.send_message(message.channel, "{} {} :gun: ".format(member.mention, hit))
-
-                if bullets[i] == 1:
-                    break
-
-            started.pop(started.index(message.channel.id))
-            yield from client.send_message(message.channel, "**GAME OVER**")
-
+            yield from roulette.start()
         else:
-            yield from client.send_message(message.channel, "This channel is already playing Russian Roulette.")
+            yield from client.send_message(message.channel, "**This channel is already playing.**")
+
+    if args[0].lower() == "!hotpotato":
+        if message.channel.id not in started:
+            started.append(message.channel.id)
+            num = 6
+
+            if len(args) > 1:
+                try:
+                    num = int(args[1])
+                except ValueError:
+                    num = 6
+
+            if num < 1:
+                num = 6
+
+            hot_potato = HotPotato(client, message, num)
+
+            yield from hot_potato.start()
+        else:
+            yield from client.send_message(message.channel, "**This channel is already playing.**")
