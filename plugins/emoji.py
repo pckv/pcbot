fb5c05e623f0eaa073d5fbb6990a4a65811507d7
PC <pc@kvalvik.no>
2016-07-15 17:07:58 +0200

    Finish the emoji plugin. It should now be able to combine and resize emojies, along with caching previously converted SVG files!

--- plugins/emoji.py
+++ plugins/emoji.py
@@ -4,10 +4,11 @@
     difficult to install for windows. """
 
 import os
+from io import BytesIO
 
 import discord
 import cairosvg
-import PIL
+from PIL import Image
 
 import plugins
 from pcbot import Annotate
@@ -15,6 +16,8 @@ from pcbot import Annotate
 
 emoji_path = "plugins/twemoji21lib/"
 size = 512
+max_width = size * 4
+max_emoji = 64
 
 
 # This will be a list of all emoji SVG files. store this in
@@ -33,12 +36,75 @@ for file in os.listdir(emoji_path):
                                           b"<svg width=\"" + size_bytes + b"px\" height=\"" + size_bytes + b"px\" ")
         emoji[emoji_name] = emoji_bytes
 
+# This cache will store any PNG file for this session
+emoji_cache = {}
+
 
 @plugins.command()
 def greater(client: discord.Client, message: discord.Message, text: Annotate.CleanContent):
-    """ Gives a HUGE version of an emoji. """
+    """ Gives a **huge** version of emojies. """
+    # Convert the given text to characters
     unicode = [hex(ord(c))[2:] for c in text]
-    assert unicode[0] in emoji, "This is not an emoji."
+    
+    # Parse all unicode and load the emojies
+    parsed_emoji = []
+    combined = False
+    for i, char in enumerate(unicode):
+        if len(parsed_emoji) > max_emoji:
+            break
 
-    converted = cairosvg.svg2png(emoji[unicode[0]])
-    yield from client.send_file(message.channel, converted, filename="{}.png".format(unicode[0]))
+        if combined:
+            combined = False
+            continue
+
+        if len(unicode) > (i + 1):
+            name = "{}-{}".format(char, unicode[i + 1])
+
+            if name in emoji:
+                if name not in emoji_cache:
+                    converted = cairosvg.svg2png(emoji[name])
+                    emoji_cache[name] = converted
+                else:
+                    converted = emoji_cache[name]
+                
+                parsed_emoji.append(converted)
+                combined = True
+                continue
+
+        if char in emoji:
+            if char not in emoji_cache:
+                converted = cairosvg.svg2png(emoji[char])
+                emoji_cache[char] = converted
+            else:
+                converted = emoji_cache[char]
+
+        parsed_emoji.append(converted)
+
+    assert parsed_emoji, "I couldn't find any emoji in that text of yours."
+
+    # We now want to combine the images if there are multiples
+    # If there is no need to combine the images, send just the one
+    if len(parsed_emoji) == 1:
+        yield from client.send_file(message.channel, parsed_emoji[0], filename="emoji.png")
+        return
+
+    image_objects = [Image.open(BytesIO(b)) for b in parsed_emoji]
+    width, height = size * len(image_objects), size
+
+    # Stitch all the images together
+    image = Image.new("RGBA", (width, height))
+    for i, image_object in enumerate(image_objects):
+        image.paste(image_object, box=(i * size, 0))
+
+    # Resize the image so that the width is no higher than 2048, but only for each factor 
+    # we go higher than the max_width
+    if width > max_width:
+        scale = 1 / ((width - 1) // max_width + 1)
+        image = image.resize((int(width * scale), int(height * scale)), Image.ANTIALIAS)
+
+    # Upload the stitched image
+    buffer = BytesIO()
+    image.save(buffer, "PNG")
+    buffer.seek(0)
+    yield from client.send_file(message.channel, buffer, filename="emojies.png")
+
