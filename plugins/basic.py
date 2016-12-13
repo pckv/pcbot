f4f2503a05fdabdb9110bdbf0f5b10c808587b35
PC <pc@kvalvik.no>
2016-12-13 19:07:28 +0100

    Fix silly mistake.

--- plugins/basic.py
+++ plugins/basic.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 
 import discord
 
-from pcbot import utils, Config, Annotate, Cleverbot
+from pcbot import utils, config, Config, Annotate, Cleverbot
 import plugins
 client = plugins.client  # type: discord.Client
 
@@ -177,7 +177,7 @@ async def remove(message: discord.Message, plugin: plugin_in_req, req_id: get_re
 @plugins.event(bot=True)
 async def on_message(message: discord.Message):
     # Have cleverbot respond to our bot
-    if not message.content.startswith("!") and client.user in message.mentions:
+    if not message.content.startswith(config.command_prefix) and client.user in message.mentions:
         # Start typing and remove the bot mention from the message.
         await client.send_typing(message.channel)
         question = " ".join(word for word in message.content.split() if not word == message.server.me.mention)
