8a39a4e12d33c3f696f17807737ed549fdd15ea2
PcBoy111 <pc@kvalvik.no>
2017-02-13 19:54:27 +0100

    Remove cleverbot support as there is now an OFFICIAL API. I won't actually remove any code, however I'm commenting out the code to deactivate it. Further explained in the comments of basic.py.

--- pcbot/cleverbot.py
+++ pcbot/cleverbot.py
@@ -1,4 +1,7 @@
-""" A port of https://github.com/folz/cleverbot.py to asynchronous python. """
+""" A port of https://github.com/folz/cleverbot.py to asynchronous python.
+
+THIS IS OUTDATED AND WILL NOT WORK.
+"""
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
--- plugins/basic.py
+++ plugins/basic.py
@@ -196,14 +196,19 @@ async def remove(message: discord.Message, plugin: plugin_in_req, req_id: get_re
     await client.say(message, "Removed feature with `{}` id **#{}**.".format(plugin, req_id + 1))
 
 
-@plugins.event(bot=True)
-async def on_message(message: discord.Message):
-    """ Have cleverbot respond to our bot. """
-    if not message.content.startswith(config.command_prefix) and client.user in message.mentions:
-        # Start typing and remove the bot mention from the message.
-        await client.send_typing(message.channel)
-        question = " ".join(word for word in message.content.split() if not word == message.server.me.mention)
+# NOTE: the code below used to be for triggering cleverbot responses on mentions. It has been commented out
+#       after the unofficial API died as a direct result of the new official API. Since the official API requires
+#       signup to use (which needs your address, email and phone number), I have discontinued cleverbot support,
+#       at least for now.
 
-        # Ask cleverbot the given question and send the response.
-        await client.say(message, await cleverbot.ask(question))
-        return True
+# @plugins.event(bot=True)
+# async def on_message(message: discord.Message):
+#     """ Have cleverbot respond to our bot. """
+#     if not message.content.startswith(config.command_prefix) and client.user in message.mentions:
+#         # Start typing and remove the bot mention from the message.
+#         await client.send_typing(message.channel)
+#         question = " ".join(word for word in message.content.split() if not word == message.server.me.mention)
+#
+#         # Ask cleverbot the given question and send the response.
+#         await client.say(message, await cleverbot.ask(question))
+#         return True
