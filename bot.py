f30f9b3b98990dd0a1c053f191eecf59082b562c
PC <pc@kvalvik.no>
2017-04-21 11:41:54 +0200

    Massively improve my previous discord.Member and discord.Channel annotation feature.

--- bot.py
+++ bot.py
@@ -148,9 +148,9 @@ async def execute_command(command: plugins.Command, message: discord.Message, *a
 def default_self(anno, default, message: discord.Message):
     """ A silly function to make Annotate.Self work. """
     if default is utils.Annotate.Self:
-        if anno is utils.Annotate.Member or anno is discord.Member:
+        if anno is utils.Annotate.Member:
             return message.author
-        elif anno is utils.Annotate.Channel or anno is discord.Channel:
+        elif anno is utils.Annotate.Channel:
             return message.channel
 
     return default
@@ -168,6 +168,11 @@ async def parse_annotation(param: inspect.Parameter, default, arg: str, index: i
         content = lambda s: utils.split(s, maxsplit=index)[-1].strip("\" ")
 
         # Valid enum checks
+        if anno is discord.Member:
+            anno = utils.Annotate.Member
+        elif anno is discord.Channel:
+            anno = utils.Annotate.Channel
+
         if isinstance(anno, utils.Annotate):
 
             if anno is utils.Annotate.Content:  # Split and get raw content from this point
@@ -178,9 +183,9 @@ async def parse_annotation(param: inspect.Parameter, default, arg: str, index: i
                 return content(message.clean_content) or default
             elif anno is utils.Annotate.LowerCleanContent:  # Lowercase of above check
                 return content(message.clean_content).lower() or default
-            elif anno is utils.Annotate.Member or anno is discord.Member:  # Checks member names or mentions
+            elif anno is utils.Annotate.Member:  # Checks member names or mentions
                 return utils.find_member(message.server, arg) or default_self(anno, default, message)
-            elif anno is utils.Annotate.Channel or anno is discord.Channel:  # Checks text channel names or mentions
+            elif anno is utils.Annotate.Channel:  # Checks text channel names or mentions
                 return utils.find_channel(message.server, arg) or default_self(anno, default, message)
             elif anno is utils.Annotate.VoiceChannel:  # Checks voice channel names or mentions
                 return utils.find_channel(message.server, arg, channel_type="voice")
