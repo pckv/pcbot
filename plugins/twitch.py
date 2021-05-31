d9b96b6e6d18691b356acd4b20f73bbae2d5daa6
jeglet <jegler@heckin.how>
2021-05-31 12:49:43 +0200

    Update twitchlib and twitch.py to new discord

---
+++ plugins/twitch.py
@@ -0,0 +1,126 @@
+""" Twitch plugin for twitch notifications.
+
+More to come?
+
+Commands:
+    twitch
+"""
+
+import discord
+import logging
+from datetime import datetime, timedelta
+
+import bot
+from pcbot import utils, Config
+import plugins
+from plugins.twitchlib import twitch
+client = plugins.client  # type: discord.Client
+
+
+twitch_config = Config("twitch-config", data=dict(guilds={}))
+
+# Keep track of all {member.id: date} that are streaming
+stream_history = {}
+repeat_notification_delta = timedelta(hours=2)
+
+
+async def on_reload(name):
+    global stream_history
+    local_history = stream_history
+
+    await plugins.reload(name)
+
+    stream_history = local_history
+
+
+@plugins.command(name="twitch")
+async def twitch_group(message: discord.Message, _: utils.placeholder):
+    """ Administrative commands for twitch functions. Notifies when discord says you're streaming. """
+    pass
+
+
+@twitch_group.command(name="channels", permissions="manage_guild")
+async def notify_channels(message: discord.Message, *channels: discord.TextChannel):
+    """ Specify channels to notify when a member goes live, or use no arguments to disable. """
+    if str(message.guild.id) not in twitch_config.data["guilds"]:
+        twitch_config.data["guilds"][str(message.guild.id)] = {}
+
+    twitch_config.data["guilds"][str(message.guild.id)]["notify_channels"] = [c.id for c in channels]
+    twitch_config.save()
+
+    # Tell the user if notifications were disabled
+    assert channels, "**Disabled stream notifications in this guild.**"
+
+    await bot.client.say(message, "**Notifying streams in:** {}".format(utils.format_objects(*channels, sep=" ")))
+
+
+def make_twitch_embed(member: discord.Member, response: dict):
+    """ Return an embed of the twitch stream, using the twitch api response.
+
+    :param member: Member object streaming.
+    :param response: Dict received through twitch.request("streams").
+    """
+    e = discord.Embed(title="Playing " + response["stream"]["game"], url=member.game.url,
+                      description=member.game.name, color=member.color)
+    e.set_author(name=member.display_name, url=member.game.url, icon_url=member.avatar_url)
+    e.set_thumbnail(url=response["stream"]["preview"]["small"] + "?date=" + datetime.now().ctime().replace(" ", "%20"))
+    return e
+
+
+def started_streaming(before: discord.Member, after: discord.Member):
+    """ Return True if the member just started streaming, and did not do so recently. """
+    # The member is not streaming at the moment
+    if after.game is None or not after.game.type == 1:
+        return False
+
+    # Check if they were also streaming before
+    if before.game and before.game.type == 1:
+        return False
+
+    # Update the stream history
+    previous_stream = stream_history.get(after.id)
+    stream_history[after.id] = datetime.now()
+
+    # Check that they didn't start streaming recently
+    if previous_stream and datetime.now() < (previous_stream + repeat_notification_delta):
+        return False
+
+    return True
+
+
+@plugins.event()
+async def on_member_update(before: discord.Member, after: discord.Member):
+    """ Notify given channels whenever a member goes live. """
+    # Return if the guild doesn't have any notify channels setup
+    if not twitch_config.data["guilds"].get(str(after.guild.id), {}).get("notify_channels", False):
+        return
+
+    # Make sure the member just started streaming
+    if not started_streaming(before, after):
+        return
+
+    # Tru getting the id and also log some possibly useful info during exceptions
+    try:
+        twitch_id = await twitch.get_id(after)
+    except twitch.RequestFailed as e:  # Could not find the streamer due to a request error
+        logging.info("Could not get twitch id of {}: {}".format(after, e))
+        return
+    except twitch.UserNotResolved as e:  # Ignore them if the id was not found.
+        logging.debug(e)
+        return
+
+    # Return the stream info of the specified user
+    try:
+        stream_response = await twitch.request("streams/" + twitch_id)
+    except twitch.RequestFailed as e:
+        logging.info("Could not get twitch stream of {} (id: {}): {}".format(after, twitch_id, e))
+        return
+
+    # If the member isn't actually streaming, return (should not be the case as discord uses the twitch api too)
+    if stream_response["stream"] is None:
+        return
+
+    # Create the embedded message and send it to every stream channel
+    embed = make_twitch_embed(after, stream_response)
+    for channel_id in twitch_config.data["guilds"][str(after.guild.id)]["notify_channels"]:
+        await bot.client.send_message(after.guild.get_channel(channel_id), embed=embed)
---
+++ plugins/twitchlib/twitch.py
@@ -0,0 +1,85 @@
+""" API wrapper for twitch.tv. """
+
+import re
+
+import discord
+
+from pcbot import utils, Config
+
+twitch_config = Config("twitch-api", data=dict(ids={}, client_id=None))
+
+# Define twitch API info
+client_id = twitch_config.data["client_id"] or ""
+api_url = "https://api.twitch.tv/kraken/"
+
+url_pattern = re.compile(r"^https://www.twitch.tv/(?P<name>.+)$")
+
+
+class RequestFailed(Exception):
+    """ For when the api request fails. """
+    pass
+
+
+class UserNotResolved(Exception):
+    """ For when a name isn't resolved. """
+    pass
+
+
+async def request(endpoint: str = None, **params):
+    """ Perform a request using the twitch kraken v5 API.
+
+    If the url key is not given, the request is sent to the root URL.
+
+    :param endpoint: The endpoint to request from, e.g users would be /kraken/users.
+    :param params: Any parameters to pass to the URL.
+    :raises RequestFailed: Generic error when the request is refused.
+    """
+    headers = {"Client-ID": client_id, "Accept": "application/vnd.twitchtv.v5+json"}
+    response = await utils.download_json(api_url + (endpoint or ""), headers=headers, **params)
+
+    # Raise an Exception when the request was invalid
+    if "error" in response:
+        raise RequestFailed(response["message"])
+
+    return response
+
+
+async def get_id(member: discord.Member, name: str = None):
+    """ Return a member's twitch user ID.
+
+    If the name kwarg is omitted, the member should be connected to discord and
+    already be streaming, so that member.game.url can be used. When omitted and
+    the member isn't streaming, a ValueError is raised.
+
+    :param member: The member to return the ID for.
+    :param name: Optionally specified twitch username of the member.
+    :raises RequestFailed: Generic error when the request is refused.
+    :raises UserNotResolved: Can't resolve member's twitch user.
+    """
+    # Return the cached id if this member has been checked before
+    if str(member.id) in twitch_config.data["ids"]:
+        return twitch_config.data["ids"][member.id]
+
+    # Try getting the name from the game url if name is not specified
+    if not name:
+        # Raise NameResolveError if the name is unknown
+        if not member.game or not member.game.url:
+            raise UserNotResolved("Could not resolve twitch name of {}: they are not streaming.".format(member))
+
+        # Attempt finding the twitch name using the discord.Game object url
+        match = url_pattern.match(member.game.url)
+        if match is None:
+            raise UserNotResolved("Could not resolve twitch name of {}: their url is broken.".format(member))
+        name = match.group("name")
+
+    # Make a request for the user found
+    response = await request("users", login=name)
+    if response["_total"] == 0:
+        raise UserNotResolved(
+            "Could not resolve twitch user account of {}: twitch user {} does not exist.".format(member, name))
+
+    # Save and return the id
+    twitch_id = response["users"][0]["_id"]
+    twitch_config.data["ids"][str(member.id)] = twitch_id
+    twitch_config.save()
+    return twitch_id
