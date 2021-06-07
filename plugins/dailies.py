86324e754f08abc489b9ad3aa5183087e54203de
jeglet <jegler@heckin.how>
2021-06-08 01:06:33 +0200

    Use member instead of message.author when finding members to clarify help result

--- plugins/dailies.py
+++ plugins/dailies.py
@@ -92,11 +92,11 @@ async def year(message: discord.Message, member: discord.Member = Annotate.Self)
 
 
 @plugins.command()
-async def meotey(message: discord.Message):
+async def meotey(message: discord.Message, member: discord.Member = Annotate.Self):
     """ Shows the daily emote for you or for the given member. """
     args = utils.split(message.content)
     d = datetime.now()
-    m = utils.find_member(message.guild, " ".join(args[1:]) or message.author.mention)
+    m = utils.find_member(message.guild, " ".join(args[1:]) or member.mention)
     if m is None:
         await client.send_message(message.channel, "**Found no such member.**")
         return
@@ -108,11 +108,11 @@ async def meotey(message: discord.Message):
 
 
 @plugins.command()
-async def meoji(message: discord.Message):
+async def meoji(message: discord.Message, member: discord.Member = Annotate.Self):
     """ Shows the daily emoji for you or for the given member. """
     args = utils.split(message.content)
     d = datetime.now()
-    m = utils.find_member(message.guild, " ".join(args[1:]) or message.author.mention)
+    m = utils.find_member(message.guild, " ".join(args[1:]) or member.mention)
     if m is None:
         await client.send_message(message.channel, "**Found no such member.**")
         return
