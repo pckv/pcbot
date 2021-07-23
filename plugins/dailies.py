8ab02fbf22fe56e59ade63cc98439a13f94ef197
Jeglet <jegler@heckin.how>
2021-07-23 15:50:13 +0200

    Fix warning when using meotey

--- plugins/dailies.py
+++ plugins/dailies.py
@@ -100,9 +100,10 @@ async def meotey(message: discord.Message, member: discord.Member = Annotate.Sel
         await client.send_message(message.channel, "**Found no such member.**")
         return
     random.seed(seed_for_member(member, date))
+    random_emote = str(random.choice(client.emojis))
     await client.send_message(message.channel, "__**{}** emote of the day__".format(
         "Your" if m == message.author else "{}'s".format(m.display_name)))
-    await client.send_message(message.channel, random.choice(client.emojis))
+    await client.send_message(message.channel, random_emote)
 
 
 @plugins.command()
